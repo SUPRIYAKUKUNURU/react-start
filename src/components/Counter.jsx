5c9746f5b4acb97f8af9ce62b44bb28e1791b1fc
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-14 21:47:07 +0530

    Counter code

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>Counter</div>
-  )
+    <>
+      <h1>Counter : {count}</h1>
+
+      <button onClick={() => setCount(count + 1)}></button>
+
+      <button>onClick={() => setCount(count - 1)}</button>
+    </>
+  );
 }
 
-export default Counter
+export default Counter;
