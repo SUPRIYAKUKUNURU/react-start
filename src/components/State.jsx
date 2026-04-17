b9cec946c86f2da1b81d17b825d207860206cd40
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-17 20:58:00 +0530

    useState

---
+++ src/components/State.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useState} from 'react' 
+
+function State() {
+    const [state ,SetState] = useState(0);
+          return (
+    <div>State</div>
+  )
+}
+
+export default State
