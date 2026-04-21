f837c3f3e4905865ce030c4b7c742e75d4a22b2b
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-21 07:39:29 +0530

    useState def

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {useState} from 'react' 
 
 //state it is object ..objects are mutable
+//useState is a built in react hook it is used to store a mutable value that changes after sometime
+//when state value chnages react automatically rerenders the component to show the updated ui on the screeen
 
 function State() {
     const [state ,SetState] = useState(0);
