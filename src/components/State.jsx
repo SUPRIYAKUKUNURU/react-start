c7591b59f5d1d72137983937d93b2bcd2101af97
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-21 07:36:52 +0530

    state def

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {useState} from 'react' 
 
+//state it is object ..objects are mutable
+
 function State() {
     const [state ,SetState] = useState(0);
           return (
