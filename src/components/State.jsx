d1378561c7daff0e0566851dd14201018ac41027
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-21 17:39:26 +0530

    hoisting

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -4,6 +4,7 @@ import {useState} from 'react'
 //state it is object ..objects are mutable
 //useState is a built in react hook it is used to store a mutable value that changes after sometime
 //when state value chnages react automatically rerenders the component to show the updated ui on the screeen
+// hoisting in javascript moves the declaration functions and variables to the top of the scope before code execution.
 
 function State() {
     const [state ,SetState] = useState(0);
