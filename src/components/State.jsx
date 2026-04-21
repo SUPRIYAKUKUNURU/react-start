3bd27dde0b9b5a89a4569213caeea030f83f76b5
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-21 10:57:37 +0530

    react

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useState} from 'react' 
-
+// react is a open source javascript library used to build user interface especially for single page applications(SPA)
 //state it is object ..objects are mutable
 //useState is a built in react hook it is used to store a mutable value that changes after sometime
 //when state value chnages react automatically rerenders the component to show the updated ui on the screeen
