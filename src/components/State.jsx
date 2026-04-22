bacda1c463c89cbc7475b9ca6f35491a2e0df4bd
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-22 19:16:47 +0530

    useState

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useState} from 'react' 
+import { useEffect } from 'react'
 // react is a open source javascript library used to build user interface especially for single page applications(SPA)
 //state it is object ..objects are mutable
 //useState is a built in react hook it is used to store a mutable value that changes after sometime
@@ -7,6 +8,9 @@ import {useState} from 'react'
 // hoisting in javascript moves the declaration functions and variables to the top of the scope before code execution.
 
 function State() {
+  useEffect(()=>{
+    console.log("useEffect is called")
+  },[])
     const [state ,SetState] = useState(0);
           return (
     <div>States</div>
