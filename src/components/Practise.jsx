321ea1e7c1dc2514a4b66d39dc4d47b92babb558
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-24 11:50:27 +0530

    Input type

--- src/components/Practise.jsx
+++ src/components/Practise.jsx
@@ -7,6 +7,8 @@ function Practise() {
     {/* Always we should close the tag in react  */}
     <img src ="photo.jpg" alt ="photo" />
     <h1>Hello {name} ...</h1>
+    <label htmlFor='Name' value={name}>Name</label>
+    <input type = "text" placeholder='Enter FullName' />
     </>
   )
 }
