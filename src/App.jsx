40fa93a91d728168e8baf7c5cb5befd07c37ba29
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-24 11:46:54 +0530

    Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Counter from './components/Counter'
 import Child from './components/Child'
-
+import Practise from './components/Practise'
 function App() {
   return (
     <>
     <Counter/>
     <Child name = "Supriya" status = "React Learner"  />
+    <Practise />
     
     </>
   )
---
+++ src/components/Practise.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Practise() {
+    const name ="react"
+  return (
+    <>
+    {/* Always we should close the tag in react  */}
+    <img src ="photo.jpg" alt ="photo" />
+    <h1>Hello {name} ...</h1>
+    </>
+  )
+}
+
+export default Practise
