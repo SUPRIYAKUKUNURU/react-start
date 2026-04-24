b8f45ca4682b51fd37ea8cae440497eb260ec393
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2026-04-24 11:52:59 +0530

    htmlFor

--- src/components/Practise.jsx
+++ src/components/Practise.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 function Practise() {
-    const name ="react"
+    const name ="React"
   return (
     <>
     {/* Always we should close the tag in react  */}
     <img src ="photo.jpg" alt ="photo" />
     <h1>Hello {name} ...</h1>
     <label htmlFor='Name' value={name}>Name</label>
-    <input type = "text" placeholder='Enter FullName' />
+    <input type = "text" placeholder='Enter FullName'  value={name} />
     </>
   )
 }
